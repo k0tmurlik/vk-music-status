f5a261e64af6ca892209cef342f55ed8f7b1a520
Vlad Surkov <tech@ltlp.ru>
2022-05-15 20:23:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json, requests, time
 
-print("Запуск")
+print("Запуск скрипта")
 
 with open("config.json", "r") as config:
     config = json.loads(config.read())
@@ -9,7 +9,7 @@ session = requests.Session()
 
 def main(audio=config['audio'], access_token=config['access_token'], v='5.131'):
     session.get(f"https://api.vk.com/method/audio.setBroadcast?audio={audio}&access_token={access_token}&v={v}")
-    time.sleep(220) # Обновление статуса каждую минуту (желательно выставить время музыки)
+    time.sleep(60) # Обновление статуса каждую минуту (желательно выставить время музыки)
 
 while True:
     main()
